06b872993912baa1df5a8fc248cb2ca7c78aed8e
aluno <aluno@etec.sp.gov.br>
2023-09-01 23:06:06 -0300

    rolou o dado

---
+++ MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace DiceRoller;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		Random random= new Random();
+		labelValue.Text = random.Next(1,11).ToString();
+	}
+}
+
